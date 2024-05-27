7174b964c028817832e14b511a3bacefb92b53d5
dunkelheitEXE <alangmoreno02@gmail.com>
2024-05-27 10:00:19 -0600

    NavbarStyle

--- index.php
+++ index.php
@@ -3,9 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="static/css/style.css">
     <title>Home</title>
 </head>
 <body>
-    <h1>Index</h1>
+    <nav class="navbar bg-dark">
+        <ul class="text-light">
+            <li><a href="">Home</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Products</a></li>
+        </ul>
+        <ul class="text-light">
+            <li><a href="">Hola</a></li>
+        </ul>
+    </nav>
+    <div class="bg-dark">
+        <img src="static/img/logo2.png" alt="" style="width: 300px;">
+    </div>
 </body>
 </html>
