1e4a648d9675496b158d07c59d94f4c05d788a63
dunkelheitEXE <alangmoreno02@gmail.com>
2024-05-30 10:44:41 -0600

    remakingNavbar

--- index.php
+++ index.php
@@ -3,32 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="static//css//style.css">
+    <link rel="stylesheet" href="static//css/style.css">
     <title>Home</title>
 </head>
 <body>
-    <div class="navbar bg-dark">
-        <nav>
-            <ul class="text-light">
-                <li><a href="">Home</a></li>
-                <ul class="collapse-section">
-                    <li><a href="">Contact</a></li>
-                    <li><a href="">Products</a></li>
-                </ul>
-            </ul>
-            <ul class="text-light">
-                <li><a href="#" class="btn btn-success btn-nav" id="navBtn">Hola</a></li>
-            </ul>
-        </nav>
-        <ul class="collapse text-light" id="collapse">
-            <li><a href="">Contact</a></li>
-            <li><a href="">Products</a></li>
+    <nav class="navbar bg-dark text-light">
+        <a href="#" class="logo"><img src="static/img/logo3.png" alt=""></a>
+        <ul>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Products</a></li>
         </ul>
-    </div>
-    
-    <div class="bg-dark">
-        <img src="static/img/logo2.png" alt="" style="width: 300px;">
-    </div>
+    </nav>
+    <ul class="collapse text-black">
+        <li><a href="#">service</a></li>
+        <li><a href="#">service</a></li>
+    </ul>
 
     <script src="static/js/style.js"></script>
 </body>
