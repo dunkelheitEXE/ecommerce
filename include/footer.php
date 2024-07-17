e538c07689da4dd220193b17856a96491fc69458
dunkelheitEXE <alangmoreno02@gmail.com>
2024-07-16 22:35:41 -0600

    Replacement

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,4 @@
 <script src="scripts/JQuery.js"></script>
-<script src="static/bootstrap/js/bootstrap.min.js"></script>
 <script src="static/bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
